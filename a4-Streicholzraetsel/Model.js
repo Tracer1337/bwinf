3d6a4b3d00c23dd2ea7106d98293bcf8070f4686
Merlin Moelter <joebau.0815@googlemail.com>
2020-09-06 15:13:49 +0200

    Solve a4 (almost -.-)

--- a4-Streicholzraetsel/Model.js
+++ a4-Streicholzraetsel/Model.js
@@ -1,10 +1,35 @@
 const Vector2d = require("../lib/Vector2d.js")
 
-const SIZE_FACTOR = 100
 const STROKE_WIDTH = .02
+const FLOATING_POINT_PRECISION = 100
+const SVG_MARGIN = 0.1
+const SVG_SIZE_FACTOR = 250
 
 function degToRad(degrees) {
     return degrees * Math.PI / 180
+}
+
+function generateSVGTag(elements) {
+    const xs = elements.map(e => [e.from, e.to].map(vector => vector.value[0])).flat()
+    const ys = elements.map(e => [e.from, e.to].map(vector => vector.value[1])).flat()
+
+    const minX = Math.min(...xs)
+    const maxX = Math.max(...xs)
+
+    const minY = Math.min(...ys)
+    const maxY = Math.max(...ys)
+
+    const width = Math.abs(minX) + Math.abs(maxX) + STROKE_WIDTH / 2
+    const height = Math.abs(minY) + Math.abs(maxY) + STROKE_WIDTH / 2
+
+    return `
+        <svg
+            width="${width * SVG_SIZE_FACTOR}"
+            height="${height * SVG_SIZE_FACTOR}"
+            viewBox="${minX - SVG_MARGIN} ${minY - SVG_MARGIN} ${width + SVG_MARGIN * 2} ${height + SVG_MARGIN * 2}"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+    `
 }
 
 class Element {
@@ -12,39 +37,34 @@ class Element {
         this.from = from
         this.to = to
     }
-
-    equals(element) {
-        if (this.from.equals(element.from) && this.to.equals(element.to)) {
-            return true
-        }
-        
-        if (this.from.equals(element.to) && this.to.equals(element.from)) {
-            return true
-        }
-
-        return false
-    }
 }
 
 class Model {
-    static getDiff(firstModel, secondModel) {
-        const diff = []
-
-        for (let firstElement of firstModel.elements) {
-            for (let secondElement of secondModel.elements) {
-                if (!firstElement.equals(secondElement)) {
-                    diff.push(secondElement)
-                }
-            }
-        }
-
-        return diff
-    }
-
     static fromElements(elements) {
         const model = new Model([])
         model.elements = elements
         return model
+    }
+
+    static generateSolution(fromModel, toModel) {
+        function renderElements(elements, color) {
+            return elements.map(({ from, to }) => (
+                `<path d="M ${from.value[0]} ${from.value[1]} L ${to.value[0]} ${to.value[1]}" stroke="${color}" stroke-width="${STROKE_WIDTH}"/>`
+            )).join("\n")
+        }
+
+        return `
+            ${generateSVGTag([...fromModel.elements, ...toModel.elements])}
+                ${renderElements(fromModel.elements, "rgba(255, 0, 0, .5)")}
+                ${renderElements(toModel.elements, "rgba(0, 255, 0, .5)")}
+
+                ${renderElements(fromModel.elements, "rgba(255, 0, 0, .5)")}
+                ${renderElements(toModel.elements, "rgba(0, 255, 0, .5)")}
+
+                ${renderElements(fromModel.elements, "rgba(255, 0, 0, .5)")}
+                ${renderElements(toModel.elements, "rgba(0, 255, 0, .5)")}
+            </svg>
+        `
     }
 
     constructor(lines) {
@@ -67,8 +87,8 @@ class Model {
 
             const endPosition = startPosition.clone().add(new Vector2d([1, 0]).rotate(degToRad(-move.angle)))
 
-            startPosition.roundTo(1000)
-            endPosition.roundTo(1000)
+            startPosition.roundTo(FLOATING_POINT_PRECISION)
+            endPosition.roundTo(FLOATING_POINT_PRECISION)
 
             this.elements.push(new Element(startPosition, endPosition))
 
@@ -77,23 +97,15 @@ class Model {
     }
 
     generateSVG() {
-        const xs = this.elements.map(e => [e.from, e.to].map(vector => vector.value[0])).flat()
-        const ys = this.elements.map(e => [e.from, e.to].map(vector => vector.value[1])).flat()
-        
-        const minX = Math.min(...xs)
-        const maxX = Math.max(...xs)
-        
-        const minY = Math.min(...ys)
-        const maxY = Math.max(...ys)
-        
-        const width = Math.abs(minX) + Math.abs(maxX) + STROKE_WIDTH / 2
-        const height = Math.abs(minY) + Math.abs(maxY) + STROKE_WIDTH / 2
-
         return `
-            <svg width="${width * SIZE_FACTOR}" height="${height * SIZE_FACTOR}" viewBox="${minX} ${minY} ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+            ${generateSVGTag(this.elements)}
                 ${this.elements.map(({ from, to }) => (
                     `<path d="M ${from.value[0]} ${from.value[1]} L ${to.value[0]} ${to.value[1]}" stroke="black" stroke-width="${STROKE_WIDTH}"/>`
-                )).join("\n    ")}
+                    // + `
+                    // <text x="${from.value[0]}" y="${from.value[1]}" font-size=".1">${from.value}</text>
+                    // <text x="${to.value[0]}" y="${to.value[1]}" font-size=".1">${to.value}</text>
+                    // `
+                )).join("\n")}
             </svg>
         `
     }
--- a4-Streicholzraetsel/index.js
+++ a4-Streicholzraetsel/index.js
@@ -15,9 +15,11 @@ const DATA_DIR = path.join(__dirname, "beispieldaten", "" + inputFileNumber)
 const fromModel = convertTXTToModel(path.join(DATA_DIR, "from.txt"))
 const toModel = convertTXTToModel(path.join(DATA_DIR, "to.txt"))
 
+if (fromModel.elements.length < toModel.elements.length) {
+    throw new Error("Das Rätsel ist nicht lösbar, da nachher mehr Streichhölzer vorhanden sein sollen als vorher.")
+}
+
 fs.writeFileSync(path.join(DATA_DIR, "from.svg"), fromModel.generateSVG())
 fs.writeFileSync(path.join(DATA_DIR, "to.svg"), toModel.generateSVG())
 
-const diff = Model.getDiff(fromModel, toModel)
-
-fs.writeFileSync(path.join(__dirname, "test.svg"), Model.fromElements(diff).generateSVG())
+fs.writeFileSync(path.join(DATA_DIR, "solution.svg"), Model.generateSolution(fromModel, toModel))
